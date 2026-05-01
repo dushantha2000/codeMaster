c60dc204567aa8e484d1f00b512628167d8c5019
Dushantha Majith <155352234+dushantha2000@users.noreply.github.com>
2026-05-02 02:36:26 +0530

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,3 +18,26 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })->create();
 
+
+
+    // server side issue solve
+
+//     <?php
+
+
+// use Illuminate\Foundation\Application;
+// use Illuminate\Foundation\Configuration\Middleware;
+ 
+// return Application::configure(basePath: dirname(__DIR__))
+//     ->withRouting(
+//         web: __DIR__.'/../routes/web.php',
+//         commands: __DIR__.'/../routes/console.php',
+//     )
+//     ->withMiddleware(function (Middleware $middleware): void {
+//         $middleware->validateCsrfTokens(except: [
+//             'stripe/*',
+//             'webhooks/*',
+//             'api/external/*',
+//         ]);
+//     })
+//     ->create();
